b036f23e61ee9bf9d8957a4f549f457c21fe156e
Alexandre Collet <3273308+AlexandreCollet@users.noreply.github.com>
2025-06-23 00:50:11 +0200

    config font rubik

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Rubik } from 'next/font/google';
+
+export const rubik = Rubik({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-rubik',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { rubik } from "@/app/fonts";
 
 export const metadata: Metadata = {
   title: "Organiz3D",
@@ -36,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rubik.variable} antialiased`}
       >
         {children}
       </body>
