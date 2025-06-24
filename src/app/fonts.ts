a01a8acfd53edac862bf1da0d15a8ed4c5dbece5
Alexandre Collet <3273308+AlexandreCollet@users.noreply.github.com>
2025-06-24 19:08:55 +0200

    reformat

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,7 +1,7 @@
-import { Rubik } from 'next/font/google';
+import { Rubik } from "next/font/google";
 
 export const rubik = Rubik({
-    subsets: ['latin'],
-    weight: ['300', '400', '500', '600', '700', '800', '900'],
-    variable: '--font-rubik',
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-rubik",
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,65 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEtsy, faInstagram, faTiktok, faPinterestP } from '@fortawesome/free-brands-svg-icons'
-import clsx from 'clsx';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faEtsy,
+    faInstagram,
+    faTiktok,
+    faPinterestP,
+} from "@fortawesome/free-brands-svg-icons";
+import clsx from "clsx";
 
 export default function Home() {
-  const links = [
-    {
-      "icon": faEtsy,
-      "text": "Visiter la boutique Etsy",
-      "url": "https://www.etsy.com/fr/shop/Organiz3DFrance",
-      "isPrimary": true,
-    },
-    {
-      "icon": faInstagram,
-      "text": "Instagram",
-      "url": "https://www.instagram.com/organiz3d_/",
-      "isPrimary": false,
-    },
-    {
-      "icon": faTiktok,
-      "text": "Tiktok",
-      "url": "https://www.tiktok.com/@organiz3d_",
-      "isPrimary": false,
-    },
-    {
-      "icon": faPinterestP,
-      "text": "Pinterest",
-      "url": "https://fr.pinterest.com/Organiz3D",
-      "isPrimary": false,
-    },
-  ]
+    const links = [
+        {
+            "icon": faEtsy,
+            "text": "Visiter la boutique Etsy",
+            "url": "https://www.etsy.com/fr/shop/Organiz3DFrance",
+            "isPrimary": true,
+        },
+        {
+            "icon": faInstagram,
+            "text": "Instagram",
+            "url": "https://www.instagram.com/organiz3d_/",
+            "isPrimary": false,
+        },
+        {
+            "icon": faTiktok,
+            "text": "Tiktok",
+            "url": "https://www.tiktok.com/@organiz3d_",
+            "isPrimary": false,
+        },
+        {
+            "icon": faPinterestP,
+            "text": "Pinterest",
+            "url": "https://fr.pinterest.com/Organiz3D",
+            "isPrimary": false,
+        },
+    ];
 
-  return (
-    <nav 
-      aria-label="Liens vers les réseaux sociaux et la boutique"
-      className="w-full"
-    >
-      <ul className="flex flex-col gap-[16px] ">
-        { links.map((link) => (
-          <li key={link.text}>
-            <a 
-              className={clsx(
-                "btn btn-block btn-primary",
-                !link.isPrimary && "btn-outline",
-              )} 
-              href={link.url}
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={link.icon} className="size-[1.2em]" />
-              {link.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    return (
+        <nav
+            aria-label="Liens vers les réseaux sociaux et la boutique"
+            className="w-full"
+        >
+            <ul className="flex flex-col gap-[16px] ">
+                {links.map((link) => (
+                    <li key={link.text}>
+                        <a
+                            className={clsx(
+                                "btn btn-block btn-primary",
+                                !link.isPrimary && "btn-outline",
+                            )}
+                            href={link.url}
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon
+                                icon={link.icon}
+                                className="size-[1.2em]"
+                            />
+                            {link.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
